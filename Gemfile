243f4b5baa35ff1019316d7dd3abb3fcecc3cdee
oak072544 <oak072544@gmail.com>
2024-08-15 10:42:54 +0700

    Add rswag gem for API documentation

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 gem "devise"
 gem "devise-jwt"
+gem "rswag"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.2.0"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_14_063530) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_14_124403) do
+  create_table "jwt_denylists", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "post_id", null: false
